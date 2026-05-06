dcaa6f816c470390dce04246ce01ee00a01a8d89
julians22 <dabnerjulian@yahoo.com>
2026-05-06 18:10:28 +0700

    upd

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -64,11 +64,6 @@ class PostSeeder extends Seeder
             // create view file for the post template view path
             $view_path = resource_path('views/articles/' . $post->slug . '.blade.php');
             if (!file_exists($view_path)) {
-
-                Artisan::call('make:view', [
-                    'name' => 'contents.articles.dynamic.' . $post->slug,
-                    '--force' => true
-                ]);
                 // update the post template view path to use the new view file
                 $post->update([
                     'template_view_path' => $post->slug
