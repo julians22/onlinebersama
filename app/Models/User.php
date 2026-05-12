229f4cfea67891006027c7f0e80f3fbb9715bc1b
julians22 <dabnerjulian@yahoo.com>
2026-05-12 16:23:41 +0700

    add can accesspanel overide

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,27 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Override;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Define who can access filament panel
+     */
+    #[Override]
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@onlinebersama.com');
+    }
 
     /**
      * The attributes that are mass assignable.
