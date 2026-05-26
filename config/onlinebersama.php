da356ba2cf538a729dc3c77f57833767b5c46b15
julians22 <dabnerjulian@yahoo.com>
2026-05-26 14:01:56 +0700

    add namestudio overlay component

---
+++ config/onlinebersama.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Namestudio Overlay Enabled
+    |--------------------------------------------------------------------------
+    |
+    | This option controls whether the Namestudio overlay is enabled or not.
+    | You can set this in your ".env" file using the NAMESTUDIO_OVERLAY_ENABLED variable.
+    |
+    */
+    'namestudio_overlay_enabled' => env('NAMESTUDIO_OVERLAY_ENABLED', false),
+];
