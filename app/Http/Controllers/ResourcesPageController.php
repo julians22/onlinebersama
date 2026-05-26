7be431372c8661dfe029d70a1641a4531675720e
Syafwan000 <muhammadsyafwan04@gmail.com>
2026-05-26 10:30:45 +0700

    feat: image replace

--- app/Http/Controllers/ResourcesPageController.php
+++ app/Http/Controllers/ResourcesPageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Topic;
-use Illuminate\Http\Request;
 
 class ResourcesPageController extends Controller
 {
@@ -15,35 +14,24 @@ class ResourcesPageController extends Controller
         return view('contents.index', compact('topics'));
     }
 
-    public function show(Request $request, string $slug) {
-
+    public function show(string $slug)
+    {
         $post = Post::published()
             ->where('slug', $slug)
             ->with('topics', 'relatedArticles', 'relatedOneArticle')
             ->first();
-        $other_posts = $post->relatedArticles;
 
-        if (!$post || $post->status !== 'published') {
-            abort(404);
-        }
+        if (!$post) abort(404);
 
         $view_path = 'contents.articles.dynamic.' . $post->slug;
         if (!view()->exists($view_path)) {
             $view_path = 'contents.articles.default';
         }
 
-        $check_image_exists = file_exists(public_path('images/articles/' . $post->slug . '.jpg'));
-        if (!$check_image_exists) {
-            $image = 'images/articles/default-image.jpg';
-        } else {
-            $image = 'images/articles/' . $post->slug . '.jpg';
-        }
-
         return view('contents.show', [
             'post' => $post,
-            'other_posts' => $other_posts,
-            'view_path' => $view_path,
-            'image' => $image
+            'other_posts' => $post->relatedArticles,
+            'view_path' => $view_path
         ]);
     }
 
