20745985b2dc965392b8b4d7c877e6b5a2071fa2
Syafwan000 <muhammadsyafwan04@gmail.com>
2026-05-28 10:26:07 +0700

    feat: data analytics

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -5,7 +5,10 @@
 @section('content')
 
 <main>
-    <section class="relative">
+    {{-- Hero --}}
+    <section
+        data-analytics-level2="hero"
+        class="relative">
         <img class="hidden w-full h-150 object-cover aspect-16/5 md:block" src="{{ asset('images/hero-resources.jpg') }}" alt="">
         <img class="block w-full h-132.5 object-cover md:hidden" src="{{ asset('images/hero-resources-mobile.jpg') }}" alt="">
         <div class="top-0 left-1/2 absolute flex items-end py-10 size-full -translate-x-1/2 container md:items-center md:py-19.5">
@@ -13,84 +16,98 @@
                 <h1 class="headline-1">Panduan Belajar</h1>
                 <p class="max-w-162.5 paragraph-md">Temukan cara memaksimalkan nama domain .com Anda dengan kumpulan artikel, video, dan panduan kami.</p>
                 <div class="flex justify-start">
-                    <a href="#" class="btn-secondary font-bold py-3! px-10! text-[18px]! w-full sm:w-auto">Lihat Semua Sumber Informasi</a>
+                    <a
+                        data-analytics-name="button | Liah Semua Sumbar Informasi"
+                        href="#"
+                        class="btn-secondary font-bold py-3! px-10! text-[18px]! w-full sm:w-auto">
+                        Lihat Semua Sumber Informasi
+                    </a>
                 </div>
             </div>
         </div>
     </section>
-    <section class="max-w-403 mx-auto py-10 md:pt-10 md:pb-19.5">
+    {{-- End Hero --}}
+
+    {{-- Small Business Stories --}}
+    <section
+        data-analytics-level2="Kisah Usaha Kecil"
+        class="max-w-403 mx-auto py-10 md:pt-10 md:pb-19.5">
         <div class="space-y-3 text-center mb-6 px-4 md:mb-8 md:px-8 xl:px-0">
             <h2 class="headline-1 text-navy-blue-300">Kisah Usaha Kecil</h2>
             <p class="paragraph-md text-deep-blue-300">Lihat bagaimana bisnis menggunakan nama domain .com untuk membantu mereka mencapai tujuan. </p>
         </div>
         <div class="online-bersama-small-business-story swiper">
             <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-1" class="swiper-slide">
+                    <img data-analytics-name="video image | Startup Experience" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | Startup Experience" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-2" class="swiper-slide">
+                    <img data-analytics-name="video image | IKAT Indonesia" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | IKAT Indonesia" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-3" class="swiper-slide">
+                    <img data-analytics-name="video image | Chick Diva Geek" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | Chick Diva Geek" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-4" class="swiper-slide">
+                    <img data-analytics-name="video image | Startup Experience" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | Startup Experience" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-5" class="swiper-slide">
+                    <img data-analytics-name="video image | IKAT Indonesia" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | IKAT Indonesia" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
+                <div data-analytics-level3="carousel-6" class="swiper-slide">
+                    <img data-analytics-name="video image | Chick Diva Geek" src="{{ asset('images/placeholder-video-why-com.jpg') }}" alt="">
                     <div class="overlay-slide">
                         <h4 class="subheadline-4">IKAT Indonesia</h4>
                         <p class="paragraph-sm">Dengarkan bagaimana nama domain .com yang dikenal membantu pemilik bisnis mengembangkan bisnisnya.</p>
                     </div>
-                    <button type="button">
+                    <button data-analytics-name="play button | Chick Diva Geek" type="button">
                         <img src="{{ asset('images/icons/white-play-rounded.svg') }}" alt="">
                     </button>
                 </div>
             </div>
             <div class="swiper-pagination"></div>
         </div>
-
     </section>
-    <section class="bg-deep-blue-300">
+    {{-- End Small Business Stories --}}
+
+    {{-- Resources --}}
+    <section
+        data-analytics-level2="Telusuri Semua Sumber Informasi"
+        class="bg-deep-blue-300">
         <div class="container py-10 md:pb-19.5">
             <div class="space-y-3 text-center text-white mb-10">
                 <h2 class="headline-1">Telusuri Semua Sumber Informasi</h2>
@@ -99,7 +116,12 @@
             <livewire:resources-list :topics="$topics->toArray()" />
         </div>
     </section>
-    <section class="flex flex-col gap-21.25 py-10 max-w-250 mx-auto px-4 md:px-8 md:py-20">
+    {{-- End Resources --}}
+
+    {{-- Entrepreneurship Journey --}}
+    <section
+        data-analytics-level2="Perjalanan Kewirausahaan"
+        class="flex flex-col gap-21.25 py-10 max-w-250 mx-auto px-4 md:px-8 md:py-20">
         <div class="space-y-3 text-center">
             <h2 class="text-navy-blue-300 text-center headline-1">Perjalanan Kewirausahaan</h2>
             <p class="paragraph-md text-deep-blue-300">Mulai dari membangun tim hingga memasarkan perusahaan Anda secara efektif, wirausahawan berpengalaman Henrik Scheel dapat membantu Anda membuka potensi pola pikir kewirausahaan.</p>
@@ -124,7 +146,11 @@
             </x-displays.accordion>
         </div>
     </section>
+    {{-- End Entrepreneurship Journey --}}
+
+    {{-- Search --}}
     <section
+        data-analytics-level2="search"
         class="bg-deep-blue-300 py-16 lg:py-22">
         <div class="container flex flex-col justify-center items-center">
             <h2 class="subheadline-2 text-white text-center max-w-xs mx-auto sm:mx-0 sm:max-w-full lg:text-[40px]!">
@@ -133,6 +159,7 @@
         </div>
         <x-displays.namestudio/>
     </section>
+    {{-- End Search --}}
 </main>
 
 @endsection
