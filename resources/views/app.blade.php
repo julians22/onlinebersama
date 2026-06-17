979448544761f0e96bc9f252fbc3ac88aa60eb48
julians22 <dabnerjulian@yahoo.com>
2026-06-17 20:12:02 +0700

    revert font load

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,9 @@
     @endenv
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="{{ assetAwsUrl('onlinebersama/webfont/font.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
