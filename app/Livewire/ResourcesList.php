b89cad3b7231e8a3a863e4af875a0f1305e18ef4
julians22 <dabnerjulian@yahoo.com>
2026-06-19 13:08:02 +0700

    upd

--- app/Livewire/ResourcesList.php
+++ app/Livewire/ResourcesList.php
@@ -14,7 +14,7 @@ class ResourcesList extends Component
 
     public int $paginate = 6;
 
-    #[Url(as: 'topic', except: "")]
+    #[Url(as: 'topic')]
     public ?string $selectedTopic = "";
 
     public array $topics = [];
