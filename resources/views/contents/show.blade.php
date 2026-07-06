2cc3661e180f5ae7a51a5ab1d603c4f447aee814
Syafwan000 <muhammadsyafwan04@gmail.com>
2026-07-06 18:17:08 +0700

    feat: remove unecessary @section

--- resources/views/contents/show.blade.php
+++ resources/views/contents/show.blade.php
@@ -1,8 +1,6 @@
 @extends('app')
 
 @section('title', $post->meta_title ?? $post->title)
-
-@section('meta_title', $post->meta_title ?? $post->title)
 @section('meta_description', $post->meta_description ?? $post->title)
 @section('meta_keywords', $post->meta_keywords ?? $post->title)
 @section('meta_image', imageArticle($post->thumbnail_file_name))
