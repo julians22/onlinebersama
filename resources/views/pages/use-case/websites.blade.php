a57e073d9ea21b076bf982db2fdf9582180df314
Syafwan000 <muhammadsyafwan04@gmail.com>
2026-09-01 15:49:09 +0700

    match the title video refer video name title

--- resources/views/pages/use-case/websites.blade.php
+++ resources/views/pages/use-case/websites.blade.php
@@ -147,7 +147,7 @@
         data-analytics-level2="How to Build a Professional Website"
         class="flex items-center gap-10 py-7.5 container flex-col md:py-12.5 xl:items-start xl:gap-30 xl:flex-row">
         <div x-data class="xl:top-20 flex flex-col gap-7.5 w-full md:gap-11.5 xl:sticky">
-            <h2 class="text-navy-blue-300 headline-1 text-center">Cara Membangun Situs <span class="sm:block">Web Profesional</span></h2>
+            <h2 class="text-navy-blue-300 headline-1 text-center">Cara Membangun Situs <span class="sm:block">Web yang Profesional</span></h2>
             <div class="group relative mx-auto rounded-[10px] overflow-hidden cursor-pointer" @click="$store.videoModal.openModal('{{ config('onlinebersama.video_id.website_detail') }}')">
                 <img class="size-full aspect-video object-cover max-w-140" src="{{ asset('images/usecase-website.webp') }}" alt="">
                 <div data-analytics-name="video image | How to Build a Professional Website" class="top-0 left-0 absolute bg-gradient-video size-full"></div>
